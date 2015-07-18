35324ff9afa40b66ffd7e37daa683e97d5ea1263
Kurnia D Win <kurnia.d.win@gmail.com>
2015-07-18 14:06:58 +0700

    init

--- handlerlist.go
+++ handlerlist.go
@@ -6,7 +6,7 @@ import (
 
 type handlerList []http.Handler
 
-func Build(h ...http.Handler) handlerList {
+func BuildChain(h []http.Handler) handlerList {
 	ret := handlerList(make([]http.Handler, 0))
 	for _, v := range h {
 		if hl, ok := v.(handlerList); ok {
