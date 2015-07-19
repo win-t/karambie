21a04cc0317b4bcea7ad356fcde757dff90b6bf6
Kurnia D Win <kurnia.d.win@gmail.com>
2015-07-20 06:10:58 +0700

    update common middleware, update README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Simple webtoolkit inspired by luvit-weblit, martini/negroni, and gorilla
+# Karambie
+
+Simple webtoolkit inspired by [luvit-weblit](https://github.com/creationix/weblit), [martini](https://github.com/go-martini/martini), [negroni](https://github.com/codegangsta/negroni/), [gorilla](https://github.com/gorilla)
+
+many of source code in middleware are fork from [martini](https://github.com/go-martini/martini)
 
 TODO: update this README.md
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/win-t/karambie"
@@ -11,12 +12,12 @@ import (
 	"github.com/win-t/karambie/middleware/static"
 )
 
-func Common(staticDir string) (karambie.HandlerList, *log.Logger) {
+func Common(verboseError bool, notFound http.Handler, staticDir string) (karambie.HandlerList, *log.Logger) {
 	logger, l := logger.New(os.Stdout, "Karambie")
 	ret := karambie.List(
 		logger,
-		recovery.New(true, l),
-		karambie.Later(notfoundhandler.New(nil)),
+		recovery.New(verboseError, l),
+		karambie.Later(notfoundhandler.New(notFound)),
 	)
 	if len(staticDir) > 0 {
 		ret = ret.Add(karambie.Later(static.New(staticDir, l)))
