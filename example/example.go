274f92fbcc045185e23190728fc4e1fb93a5937d
Kurnia D Win <kurnia.d.win@gmail.com>
2015-07-21 06:03:03 +0700

    update example

--- example/example.go
+++ example/example.go
@@ -19,12 +19,15 @@ import (
 	"github.com/martini-contrib/render"
 )
 
-func hello(rw http.ResponseWriter, r *http.Request) {
+func hello(rw http.ResponseWriter, r *http.Request) { // http.HandlerFunc style
 	fmt.Fprintf(rw, "Hello World")
 }
 
-func sayhi(render render.Render, request *http.Request) {
+func sayhi(render render.Render, request *http.Request) { // martini.Handler style
+	// get parameter from gorilla routing
 	name := mux.Vars(request)["name"]
+
+	// use martini-contrib/render
 	render.JSON(http.StatusOK, "Hi "+name)
 }
 
@@ -39,7 +42,7 @@ func filterheader(rw http.ResponseWriter, r *http.Request) {
 	} else {
 		ctx := karambie.Context(rw)
 		ctx.Set("apikey", key)
-		// dont write anything, if you do that the chain list will be stop
+		// dont write anything, if you do that, the chain list will be stop
 	}
 }
 
@@ -58,41 +61,44 @@ func main() {
 	// register logger service for martini
 	martini.Map(log)
 
-	//the list is immutable, every calling to Add will create new list
-	// karambie.Later will create new handler that will be executed after succeeding handler
-	list = list.Add(logger, recovery)                // list is [logger, recovery, ...]
-	list = list.Add(karambie.Later(notfoundhandler)) // list is [logger, recovery, ..., notfoundhandler]
-	list = list.Add(karambie.Later(static))          // list is [logger, recovery, ..., static, notfoundhandler]
+	// the list is immutable, every calling to Add or AddFunc will create new list
+	list = list.Add(logger, recovery)
+	list = list.Add(karambie.Later(notfoundhandler))
+	list = list.Add(karambie.Later(static))
 	// or you can use karambie/middleware.Common() to build those list
 
+	// list is [logger, recovery, notfoundhandler, static]
+	// but the order of execution is [logger, recovery, static, notfoundhandler]
+	// karambie.Later will create new handler that will be executed after succeeding handler
 	// list processing will stop if one of them respond the request
-	// so, we can user list as NotFoundHandler (handle static file, and show error 404 if necessary)
-	route.NotFoundHandler = list
 
-	// see, the they are different list
-	secureList := list.Add(martini.Conv(auth.Basic("user", "pass"))) // secureList is [logger, recovery, auth, ..., static, notfoundhandler]
-	list = list.Add(martini.Conv(render.Renderer()))                 // list is       [logger, recovery, render, ..., static, notfoundhandler]
+	secureList := list.Add(martini.Conv(auth.Basic("user", "pass"))) // execution of secureList is [logger, recovery, auth, static, notfoundhandler]
+	list = list.Add(martini.Conv(render.Renderer()))                 // execution of list is       [logger, recovery, render, static, notfoundhandler]
+	// list != secureList, because it is immutable, every calling to Add or AddFunc will create new list
 
 	// using http.HandlerFunc style
-	// [logger, recovery, render, hello, (x)static, (x)notfoundhandler]
-	// but 'static' and 'notfoundhandler' will be ignored because 'hello' response the request
-	route.Handle("/helloworld", list.AddFunc(hello))
+	route.Handle("/helloworld", list.AddFunc(hello)) // [logger, recovery, render, hello]
+	// 'static' and 'notfoundhandler' will be ignored because 'hello' response the request
 
-	// using martini.Handler style,
-	// and gorilla routing
-	// [logger, recovery, render, sayhi, (x)static, (x)notfoundhandler]
-	route.Handle("/sayhi/{name}", list.Add(martini.Conv(sayhi)))
+	// we can user list as NotFoundHandler (handle static file, and show error 404 if necessary)
+	route.NotFoundHandler = list // [logger, recovery, static, notfoundhandler]
+	// 'notfoundhandler' will be ignored if 'static' response the request
+
+	// using martini.Handler style and gorilla routing
+	route.Handle("/sayhi/{name}", list.Add(martini.Conv(sayhi))) // [logger, recovery, render, sayhi]
 
 	// use secureList for sensitive resource
-	// [logger, recovery, auth, secret, (x)static,(x)notfoundhandler]
-	route.Handle("/secret", secureList.AddFunc(secret))
+	route.Handle("/secret", secureList.AddFunc(secret)) // [logger, recovery, auth, secret]
 
 	// add filterheader to list
-	apiList := list.AddFunc(filterheader)
+	apiList := list.AddFunc(filterheader) // execution of apiList is [logger, recovery, filterheader, static, notfoundhandler]
 	route.Handle("/api", apiList.AddFunc(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		// this handler will not be called if 'filterheader' is not passed
+
 		// get apikey
 		ctx := karambie.Context(rw)
 		key := ctx.Get("apikey").(string)
+
 		fmt.Fprintln(rw, "Your api key : "+key)
 	})))
 
